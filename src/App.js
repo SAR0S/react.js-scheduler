20efd55549c7352f4cddd53481a00765fbd680f4
SAR0S <weorbit79@gmail.com>
2021-03-21 23:02:11 +0900

    Add Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  Appointments,
+  AppointmentForm,
+  AppointmentTooltip,
+  WeekView,
+  EditRecurrenceMenu,
+  AllDayPanel,
+  ConfirmationDialog,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
 function App() {
   return (
     <div className="App">
