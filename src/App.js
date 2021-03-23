d3f61ef016bd1754d98ef645e21f63bccedfeedf
SAR0S <weorbit79@gmail.com>
2021-03-23 23:00:11 +0900

    Edit Demo.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Demo from "./Demo.js";
 
-import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
-import {
-  Scheduler,
-  Appointments,
-  AppointmentForm,
-  AppointmentTooltip,
-  WeekView,
-  EditRecurrenceMenu,
-  AllDayPanel,
-  ConfirmationDialog,
-} from '@devexpress/dx-react-scheduler-material-ui';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>스케줄러 만들기는 내일부터</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <Demo/>
+        </div>
+    );
+  }
 }
-
 export default App;
