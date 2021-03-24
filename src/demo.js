1892f7462f204a77c8888dee48c356bb603c9c77
SAR0S <weorbit79@gmail.com>
2021-03-24 22:44:58 +0900

    Edit Demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {Paper} from '@material-ui/core';
 import {ViewState, EditingState} from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
@@ -12,53 +12,112 @@ import {
     ConfirmationDialog
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-export default class demo extends Component {
+export default class Demo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [{
-                title: 'Practice Ollie',
-                startDate: new Date(2021, 3, 22, 9, 35),
-                endDate: new Date(2021, 3, 22, 11, 30),
-                id: 0,
-                location: 'Skate Park',
-            }, {
-                title: 'Buy a new skateboard',
-                startDate: new Date(2021, 3, 23, 12, 11),
-                endDate: new Date(2021, 3, 23, 13, 0),
-                id: 1,
-                location: 'Skateboard Shop',
-            }, {
-                title: 'learn how to Ollie',
-                startDate: new Date(2021, 3, 25, 14, 30),
-                endDate: new Date(2021, 3, 25, 15, 35),
-                id: 2,
-                location: 'Skate Park',
-            }],
+            data: [
+                {
+                    title: 'Practice Ollie',
+                    startDate: new Date(2021, 3, 22, 9, 35),
+                    endDate: new Date(2021, 3, 22, 11, 30),
+                    id: 0,
+                    location: 'Skate Park'
+                }, {
+                    title: 'Buy a new skateboard',
+                    startDate: new Date(2021, 3, 23, 12, 11),
+                    endDate: new Date(2021, 3, 23, 13, 0),
+                    id: 1,
+                    location: 'Skateboard Shop'
+                }, {
+                    title: 'learn how to Ollie',
+                    startDate: new Date(2021, 3, 25, 14, 30),
+                    endDate: new Date(2021, 3, 25, 15, 35),
+                    id: 2,
+                    location: 'Skate Park'
+                }
+            ],
             currentDate: '2021-03-22',
-        
+
             addedAppointment: {},
             appointmentChanges: {},
-            editingAppointmentId: undefined,
+            editingAppointmentId: undefined
         };
-    
+
         this.commitChanges = this.commitChanges.bind(this);
         this.changeAddedAppointment = this.changeAddedAppointment.bind(this);
         this.changeAppointmentChanges = this.changeAppointmentChanges.bind(this);
         this.changeEditingAppointmentId = this.changeEditingAppointmentId.bind(this);
     }
 
+    changeAddedAppointment(addedAppointment) {
+        this.setState({addedAppointment});
+    }
+
+    changeAppointmentChanges(appointmentChanges) {
+        this.setState({appointmentChanges});
+    }
+
+    changeEditingAppointmentId(editingAppointmentId) {
+        this.setState({editingAppointmentId});
+    }
+
+    commitChanges({added, changed, deleted}) {
+        this.setState((state) => {
+            let {data} = state;
+            if (added) {
+                const startingAddedId = data.length > 0
+                    ? data[data.length - 1].id + 1
+                    : 0;
+                data = [
+                    ...data, {
+                        id: startingAddedId,
+                        ...added
+                    }
+                ];
+            }
+            if (changed) {
+                data = data.map(appointment => (
+                    changed[appointment.id]
+                        ? {
+                            ...appointment,
+                            ...changed[appointment.id]
+                        }
+                        : appointment
+                ));
+            }
+            if (deleted !== undefined) {
+                data = data.filter(appointment => appointment.id !== deleted);
+            }
+            return {data};
+        });
+    }
+
     render() {
-        const {
-            currentDate, data, addedAppointment, appointmentChanges, editingAppointmentId,
-        } = this.state;
+        const {currentDate, data, addedAppointment, appointmentChanges, editingAppointmentId} = this.state;
         return (
-            <div>
-                <Scheduler
-                    data={data}
-                    height={660}>
+            <Paper>
+                <Scheduler data={data} height={1000}>
+                    <ViewState currentDate={currentDate}/>
+                    <EditingState
+                        onCommitChanges={this.commitChanges}
+                        addedAppointment={addedAppointment}
+                        onAddedAppointmentChange={this.changeAddedAppointment}
+                        appointmentChanges={appointmentChanges}
+                        onAppointmentChangesChange={this.changeAppointmentChanges}
+                        editingAppointmentId={editingAppointmentId}
+                        onEditingAppointmentIdChange={this.changeEditingAppointmentId}/>
+                    <WeekView startDayHour={9} endDayHour={20}/>
+                    <AllDayPanel/>
+                    <EditRecurrenceMenu/>
+                    <ConfirmationDialog/>
+                    <Appointments/>
+                    <AppointmentTooltip
+                        showOpenButton="showOpenButton"
+                        showDeleteButton="showDeleteButton"/>
+                    <AppointmentForm/>
                 </Scheduler>
-            </div>
+            </Paper>
         );
     }
 }
