16f95517350522eb2bd832540a36847321b650b9
SAR0S <weorbit79@gmail.com>
2021-03-25 22:16:42 +0900

    Add plans.js

---
+++ src/demo-data/plans.js
@@ -0,0 +1,21 @@
+export const plans = [
+    {
+        title: 'Practice Ollie',
+        startDate: new Date(2021, 3, 22, 9, 35),
+        endDate: new Date(2021, 3, 22, 11, 30),
+        id: 0,
+        location: 'Skate Park'
+    }, {
+        title: 'Buy a new skateboard',
+        startDate: new Date(2021, 3, 23, 12, 11),
+        endDate: new Date(2021, 3, 23, 13, 0),
+        id: 1,
+        location: 'Skateboard Shop'
+    }, {
+        title: 'learn how to Ollie',
+        startDate: new Date(2021, 3, 25, 14, 30),
+        endDate: new Date(2021, 3, 25, 15, 35),
+        id: 2,
+        location: 'Skate Park'
+    }
+]
--- src/demo.js
+++ src/demo.js
@@ -11,32 +11,13 @@ import {
     AllDayPanel,
     ConfirmationDialog
 } from '@devexpress/dx-react-scheduler-material-ui';
+import { plans } from './demo-data/plans';
 
 export default class Demo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [
-                {
-                    title: 'Practice Ollie',
-                    startDate: new Date(2021, 3, 22, 9, 35),
-                    endDate: new Date(2021, 3, 22, 11, 30),
-                    id: 0,
-                    location: 'Skate Park'
-                }, {
-                    title: 'Buy a new skateboard',
-                    startDate: new Date(2021, 3, 23, 12, 11),
-                    endDate: new Date(2021, 3, 23, 13, 0),
-                    id: 1,
-                    location: 'Skateboard Shop'
-                }, {
-                    title: 'learn how to Ollie',
-                    startDate: new Date(2021, 3, 25, 14, 30),
-                    endDate: new Date(2021, 3, 25, 15, 35),
-                    id: 2,
-                    location: 'Skate Park'
-                }
-            ],
+            data: plans,
             currentDate: '2021-03-22',
 
             addedAppointment: {},
