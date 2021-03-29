c164d020f755bf5d26c56d4b56354d8b93cd03cf
SAR0S <weorbit79@gmail.com>
2021-03-29 23:41:55 +0900

    Edit Demo data

--- src/demo-data/plans.js
+++ src/demo-data/plans.js
@@ -2,14 +2,14 @@ export const plans = [
     {
         title: 'Practice Ollie',
         priorityId: 2,
-        startDate: '2021-03-21T09:30',
-        endDate: '2021-03-21T10:00',
+        startDate: '2021-03-22T09:30',
+        endDate: '2021-03-22T10:00',
     },
     {
         title: 'Buy a new skateboard',
         priorityId: 2,
-        startDate: '2021-03-24T10:30',
-        endDate: '2021-03-24T12:00',
+        startDate: '2021-03-23T10:30',
+        endDate: '2021-03-23T12:00',
     },
     {
         title: 'learn how to Ollie',
