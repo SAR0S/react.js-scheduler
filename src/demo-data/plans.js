68bc2c6ca89f2938772b486dd9d6bde5fae75f51
SAR0S <weorbit79@gmail.com>
2021-04-02 20:17:05 +0900

    edit demo data

--- src/demo-data/plans.js
+++ src/demo-data/plans.js
@@ -35,4 +35,10 @@ export const plans = [
         startDate: '2021-04-01T12:15',
         endDate: '2021-04-01T13:30',
     },
+    {
+        title: '내일부터는 가짜',
+        priorityId: 2,
+        startDate: '2021-04-02T12:15',
+        endDate: '2021-04-02T13:30',
+    },
 ]
